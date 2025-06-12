c49f8dc3b5adfad80853587ea7f08fd8552cd1bd
joony102910 <toisle5575@naver.com>
2025-06-12 21:10:59 +0900

    tkwlsEmldnrl.py

--- tkwlsEmldnrl.py
+++ tkwlsEmldnrl.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-## 함수 선언 부분 ##
 def loadImage(fname):
     global inImage, XSIZE, YSIZE
     fp = open(fname, 'rb')
@@ -23,16 +22,14 @@ def displayImage(image):
             data = image[i][k]
             tmpString += "#%02x%02x%02x " % (data, data, data)
         rgbString = "{" + tmpString.strip() + "}"
-        paper.put(rgbString, (0, i))  # 한 줄씩 출력
+        paper.put(rgbString, (0, i))  
 
-## 전역 변수 선언 부분 ##
 window = None
 canvas = None
 XSIZE, YSIZE = 256, 256
-inImage = []  # 2차원 리스트(메모리)
-paper = None  # 추가: PhotoImage 전역화
+inImage = []  
+paper = None  
 
-## 메인 코드 부분 ##
 window = Tk()
 window.title("흑백 사진 보기")
 canvas = Canvas(window, height=XSIZE, width=YSIZE)
@@ -41,7 +38,7 @@ canvas.pack()
 paper = PhotoImage(width=XSIZE, height=YSIZE)
 canvas.create_image((XSIZE / 2, YSIZE / 2), image=paper, state="normal")
 
-filename = "C:/Users/남영/OneDrive/바탕 화면/python/FileTest/Lenna.raw"  # 따옴표 제거, 슬래시로 변경
+filename = "C:/Users/남영/OneDrive/바탕 화면/python/FileTest/Lenna.raw"  
 loadImage(filename)
 displayImage(inImage)
 
